de5cb520e4c6ce41e934ebc1115c2ff65289f285
Andrew Jones <joneseyit@protonmail.com>
2018-11-19 18:02:37 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+#   Set a controller action to render a separate view
+# Render HTML in the view
 
+
+  get '/' do
+    erb :index
+  end
+
+  
 
 end
